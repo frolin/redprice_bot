a158153755bb67df768c7b24c9c2a24155f1267b
frolin <fralunia@gmail.com>
2021-12-05 23:45:23 +0300

    add cert, hack 4 production

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 bot: rake telegram:bot:set_webhook RAILS_ENV=production
-worker: bundle exec sidekiq -e production -C config/sidekiq.yml
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml CERT=cert.pem
 release: bundle exec rake db:migrate
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module RedpriceBot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # to use encrypted secrets in rake tasks on production env
+    config.read_encrypted_secrets = (Rails.env == 'production')
+    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
