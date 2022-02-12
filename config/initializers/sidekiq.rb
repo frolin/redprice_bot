7a7debeca13ce51f982a915b648709ced0b59b83
frolin <fralunia@gmail.com>
2022-02-12 19:13:59 +0300

    compose version

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,23 +4,17 @@ if File.exist?(schedule_file) && Sidekiq.server?
 	Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
-end
+sidekiq_config = { url: ENV['REDIS_URL'] }
 
 Sidekiq.configure_server do |config|
-
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+	config.redis = sidekiq_config
 
 	config.death_handlers << ->(job, ex) do
 		Sentry.capture_exception(ex)
 		puts "Uh oh, #{job['class']} #{job["jid"]} just died with error #{ex.message}."
 	end
+end
 
+Sidekiq.configure_client do |config|
+	config.redis = sidekiq_config
 end
